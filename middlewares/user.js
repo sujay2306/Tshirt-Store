c9792e9b1a586c51d7980d6c0151169a5e0f5b8a
sujay2306 <sujayks007@gmail.com>
2023-01-21 10:43:43 +0530

    added isloggedin middlewear

---
+++ middlewares/user.js
@@ -0,0 +1,19 @@
+const User = require("../models/user")
+const BigPromise = require("../middlewares/bigPromise");
+const CustomError = require("../utils/customErrors");
+const jwt = require("jsonwebtoken");
+const { json } = require("express");
+
+//ISLOGGEDIN MIDDELWARE
+exports.isLoggedIn = BigPromise( async(req, res, next) => {
+    const token = req.cookies.token || req.header("Authorzation").replace("Bearer ",""); //space is compulsory after Bearer and replace with nothing this will give us token
+   
+    if (!token){
+        return next (new CustomError("Login firsdt to access this page", 401));
+    }
+    //decode the token and verify
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = awaitUser.findById(decoded.id);
+    next();
+
+})
