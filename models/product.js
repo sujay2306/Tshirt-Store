df9cefdfbc0cc94da3776dd73ae023e3829b1962
sujay2306 <sujayks007@gmail.com>
2023-01-22 20:38:31 +0530

    added enums

--- models/product.js
+++ models/product.js
@@ -79,10 +79,17 @@ const productSchema  = new mongoose.Schema({
             type: String,  
             required: true
         },
+    }],
 
-        
-
-    }]
+    user : {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    createdAt : {
+        type: Date,
+        default: Date.now,
+    }
 })
 
 
